3e300ff6045ab87688265b711dd4881ad2f39c33
Trent Whitinger <twhitinger@gmail.com>
2016-09-22 08:45:48 -0600

    making progress

--- spec/features/user_can_click_on_item_spec.rb
+++ spec/features/user_can_click_on_item_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-RSpec.describe "User can find items by keyword" do
-  scenario "searching by valid keyword" do
+RSpec.describe "User can click on a item and return info" do
+  scenario "searchshow" do
     # As a user
     # When I visit "/"
 
@@ -10,8 +10,8 @@ RSpec.describe "User can find items by keyword" do
     u = User.create(email: "person@example.com", password: password, password_confirmation: password )
 
     within "#new_user" do
-     fill_in "user_email", with: u.email
-     fill_in "user_password", with: password
+      fill_in "user_email", with: u.email
+      fill_in "user_password", with: password
     end
     click_on "Log in"
     expect(current_path).to eq("/")
@@ -21,14 +21,17 @@ RSpec.describe "User can find items by keyword" do
     click_on "Find Items"
     # Then I should be on page "/search" with parameters visible in the url
     expect(current_path).to eq("/search")
+    within ".search_results" do
+      first(:link).click
+    end
+
+  
     # Then I should see a list of 10 cooking items
     # And the items  should be displayed
     # And each item should display Title, Author, Price, SalesRank, CustomerReviews
-    expect(page).to have_content("Title")
-    expect(page).to have_content("Product Group")
-    expect(page).to have_content("Manufacturer")
-    expect(page).to have_content("Price")
-    expect(page).to have_content("SalesRank")
+    expect(page).to have_content("First Response")
+    expect(page).to have_content("All Comments")
+
     # expect(page).to have_content("CustomerReviews")
   end
 end
--- spec/services/amazon_service_spec.rb
+++ spec/services/amazon_service_spec.rb
@@ -8,23 +8,27 @@ describe AmazonService do
       all_data = AmazonService.new.get_info(keyword)
       sales_rank = all_data.first["SalesRank"]
       item = all_data.first["ItemAttributes"]
-      customer_reviews_url = all_data.first["CustomerReviews"]["IFrameURL"]
-
 
       expect(item["Title"]).to eq("Thug Kitchen: The Official Cookbook: Eat Like You Give a F*ck")
       expect(item["Author"]).to eq("Thug Kitchen")
       expect(item["ListPrice"]["FormattedPrice"]).to eq("$25.99")
       expect(sales_rank).to eq(all_data.first["SalesRank"])
-      expect(customer_reviews_url).to eq(all_data.first["CustomerReviews"]["IFrameURL"])
     end
 
-    # it "returns item price for the given item upc" do
-    #
-    #   all_data = AmazonService.new.get_info(keyword)
-    #
-    #   item1, item2 = all_data[0], all_data[4]
-    #   expect(item1["ItemAttributes"]["UPC"]).to eq("041570109489")
-    #   expect(item2["ItemAttributes"]["UPC"]).to eq("041570072561")
-    # end
+    it "returns item price for the given item upc" do
+      item_id = "0766897769191"
+      item = AmazonService.new.find_item(item_id)
+
+    
+      expect(item["ItemAttributes"]["Title"]).to eq("SUPERIORBAND - Ballet Stretch Band for Dance & Gymnastics Training")
+
+    end
+
+    it "returns a url for the more search results" do
+      keyword = "cooking"
+      url = AmazonService.new.find_more_search_results(keyword)
+
+      expect(url).to eq("http://www.amazon.com/gp/redirect.html?linkCode=xm2&SubscriptionId=AKIAIKNB7T4U6I6F6WBA&location=http%3A%2F%2Fwww.amazon.com%2Fgp%2Fsearch%3Fkeywords%3Dcooking%26url%3Dsearch-alias%253Dstripbooks&tag=bivatol-20&creative=386001&camp=2025")
+    end
   end
 end

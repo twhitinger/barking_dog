69a3cf8ebbe6bd7f00999778ac8ac41696fb6ea1
Trent Whitinger <twhitinger@gmail.com>
2016-09-21 23:09:46 -0600

    settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_cable.allowed_request_origins = ['https://barking-dog.herokuapp.com']
+  config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
